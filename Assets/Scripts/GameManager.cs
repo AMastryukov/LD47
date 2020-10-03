7fccd01734b68e0c4abf14bcc492f5722fcf9bc1
Alexander Mastryukov <Alexandermastryukov@gmail.com>
2020-10-03 12:37:44 -0400

    Improve game mockup & game flow
    - Decouple Game from GameManager
    - Add timers for the game behavior
    - Improve UI mockup (and add monitor sprite)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,43 +1,128 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private float defaultTimer = 60f;
-    [SerializeField] private float defaultMotivation = 25f;
+    public static Action<float> onTimerUpdated;
+    public static Action<float> onMotivationUpdated;
 
-    public int Art { get; private set; }
-    public int Music { get; private set; }
-    public int Code { get; private set; }
-    public int Inspiration { get; private set; }
+    [SerializeField] private float defaultTimer = 48f;
+    [SerializeField] private float defaultMotivation = 25f;
+    [SerializeField] private float maximumMotivation = 100f;
+    [SerializeField] private float motivationLossRate = 2f;
+    [SerializeField] private float motivationGainRate = 10f;
+    [SerializeField] private float inspirationChance = 0.25f;
+    [SerializeField] private float inspirationTimer = 8f;
+
+    public Game CurrentGame { get; private set; }
     public float Motivation { get; private set; }
     public float Timer { get; private set; }
+    public float InspiredArtTimer { get; private set; }
+    public float InspiredMusicTimer { get; private set; }
+    public float InspiredCodeTimer { get; private set; }
 
-    public void StartNewGame()
+    private Coroutine timerCoroutine;
+    private Coroutine motivationCoroutine;
+    private Coroutine inspirationRollCoroutine;
+
+    private void Start()
     {
-        Art = 0;
-        Music = 0;
-        Code = 0;
+        NewGame();
+    }
 
-        Inspiration = 0;
+    public void NewGame()
+    {
+        CurrentGame = new Game();
 
         Motivation = defaultMotivation;
         Timer = defaultTimer;
+
+        InspiredArtTimer = 0f;
+        InspiredMusicTimer = 0f;
+        InspiredCodeTimer = 0f;
+
+        timerCoroutine = StartCoroutine(CountdownTimer());
+        motivationCoroutine = StartCoroutine(LoseMotivation());
+        inspirationRollCoroutine = StartCoroutine(InspirationRoll());
     }
 
-    public void AddArt(int art)
+    public void AddFun(float fun)
     {
-        Art += art;
+        if (CurrentGame != null) { CurrentGame.Fun += fun; }
     }
 
-    public void AddMusic(int music)
+    public void AddGraphics(float graphics)
     {
-        Music += music;
+        if (CurrentGame != null) { CurrentGame.Graphics += graphics; }
     }
 
-    public void AddCode(int code)
+    public void AddAudio(float audio)
     {
-        Code += code;
+        if (CurrentGame != null) { CurrentGame.Audio += audio; } 
+    }
+
+    private IEnumerator CountdownTimer()
+    {
+        while (Timer > 0f)
+        {
+            Timer -= 1f;
+            onTimerUpdated?.Invoke(Timer);
+
+            yield return new WaitForSeconds(1f);
+        }
+
+        FinishGame(true);
+    }
+
+    private IEnumerator LoseMotivation()
+    {
+        while (Motivation > 0f)
+        {
+            Motivation -= motivationLossRate * 0.25f;
+            onMotivationUpdated?.Invoke(Motivation / maximumMotivation);
+
+            yield return new WaitForSeconds(0.25f);
+        }
+
+        FinishGame(false);
+    }
+
+    private IEnumerator InspirationRoll()
+    {
+        while (Timer > 0f)
+        {
+            yield return new WaitForSeconds(1f);
+
+            // Roll for a chance of inspiration every second
+            if (UnityEngine.Random.Range(0f, 1f) < inspirationChance)
+            {
+                
+            }
+        }
+    }
+
+    private void FinishGame(bool completed)
+    {
+        StopCoroutine(timerCoroutine);
+        StopCoroutine(motivationCoroutine);
+        StopCoroutine(inspirationRollCoroutine);
+
+        if (completed) { Debug.Log("You submitted your game!"); }
+        else { Debug.Log("You lost all motivation and gave up"); }
+    }
+}
+
+public class Game
+{
+    public float Fun { get; set; }
+    public float Graphics { get; set; }
+    public float Audio { get; set; }
+
+    public Game()
+    {
+        Fun = 0f;
+        Graphics = 0f;
+        Audio = 0f;
     }
 }
---
+++ Assets/Scripts/MotivationDisplay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MotivationDisplay : MonoBehaviour
+{
+    [SerializeField] private Image motivationBarFill;
+
+    private void Awake()
+    {
+        GameManager.onMotivationUpdated += UpdateMotivationText;
+    }
+
+    private void OnDestroy()
+    {
+        GameManager.onMotivationUpdated -= UpdateMotivationText;
+    }
+
+    private void UpdateMotivationText(float newMotivation)
+    {
+        motivationBarFill.fillAmount = newMotivation;
+        motivationBarFill.color = new Color(2f * (1f - newMotivation), 2f * newMotivation, 0f);
+    }
+}
