f261824e0ea21767e3fa8e94dd8bffe9d0cee33a
MahadAdnanGul <64431074+MahadAdnanGul@users.noreply.github.com>
2020-10-05 22:32:49 +0500

    Update
    conflict resolution/more sound integration, bug fixes etc

---
+++ Assets/Scripts/ArtMinigame/art_sound.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class art_sound : MonoBehaviour, IPointerDownHandler
+{
+    //reference to ingamesounds
+    [SerializeField]
+    private GameObject main;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(EventSystem.current.IsPointerOverGameObject())
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("yes");
+                main.GetComponent<InGameSounds>().art_game();
+            }
+        }
+        
+    }
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        
+        
+    }
+}
