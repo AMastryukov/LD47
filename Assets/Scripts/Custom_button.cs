8aeef9046c6478b7118d053c6899dffe248924d7
MahadAdnanGul <64431074+MahadAdnanGul@users.noreply.github.com>
2020-10-05 06:56:56 +0500

    Main Menu
    Added Main Menu Scene

---
+++ Assets/Scripts/Custom_button.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Custom_button : MonoBehaviour, IPointerEnterHandler , IPointerDownHandler
+{
+    public AudioSource main;
+    public AudioClip highlight;
+    public AudioClip click;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Image>().alphaHitTestMinimumThreshold = 0.1f;
+    }
+
+    public void OnPointerEnter(PointerEventData ped)
+    {
+        Debug.Log("yes");
+        main.PlayOneShot(highlight);
+    }
+
+    public void OnPointerDown(PointerEventData ped)
+    {
+        main.PlayOneShot(click);
+    }
+
+}
