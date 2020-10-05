814e400884cb4cf029fb32278086a4cef79dceb5
Alexander Mastryukov <Alexandermastryukov@gmail.com>
2020-10-05 13:02:48 -0400

    Add mouse, coffee pot randomization script
    - Add rubber duckie too, on the left side

---
+++ Assets/Scripts/CoffeePot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class CoffeePot : MonoBehaviour
+{
+    [SerializeField] private Sprite[] coffeePotSprites;
+
+    private Image coffeePotImage;
+
+    private void Awake()
+    {
+        coffeePotImage = GetComponent<Image>();
+        coffeePotImage.sprite = coffeePotSprites[UnityEngine.Random.Range(0, coffeePotSprites.Length)];
+    }
+}
